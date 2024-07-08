5c06f5f5e695d00edb3890c399da366a9f5ac759
jubliantgathering <jubliantgathering@gmail.com>
2024-07-08 15:35:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,58 @@
+function openWin1(){
+    window.location.assign("beautician.html");
+  }
+  
+  function openWin2(){
+    window.location.assign("cloth.html");
+  }
+  
+  function openWin3(){
+    window.location.assign("convention.html");
+  }
+  
+  function openWin4(){
+    window.location.assign("invitation.html");
+  }
+  
+  function openWin5(){
+    window.location.assign("jewellery.html");
+  }
+  
+  function openWin6(){
+    window.location.assign("tent.html");
+  }
+  
+  function openWin7(){
+    window.location.assign("video.html");
+  }
+  
+  function showMsg(){
+    alert("Booking successfull");
+  }
+  
+  function openWin8(){
+    window.location.assign("c-login.html");
+  }
+  
+  function openWin9(){
+    window.location.assign("v-login.html");
+  }
+  
+  function openWinLogin(){
+    window.location.assign("index.html");
+  }
+
+  function openWinRegister(){
+    window.location.assign("v-register.html");
+  }
+
+
+
+let popup=document.getElementById("popup");
+ function openPopup(){
+  popup.classList.add("open-popup");
+} 
+
+function closePopup(){
+  popup.classList.remove("open-popup");
+} 
